6987e4b9517f39f96653041c085896d740ff4aae
Claudio Bantaloukas <cbantaloukas@devmac08.ccdc.cam.ac.uk>
2020-02-13 15:40:29 +0000

    Add scripts to build

---
+++ build-virtualbox.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+pushd $DIR
+
+echo 'creating output directory'
+mkdir -p output
+rm -rf ./output/packer-ubuntu-18.04-x86_64-virtualbox
+
+echo 'building base images'
+packer build \
+  -only=virtualbox-iso \
+  -except=vsphere,vsphere-template \
+  -var 'build_directory=./output/' \
+  -var 'disk_size=400000' \
+  -var 'cpus=2' \
+  -var 'memory=4096' \
+  -var 'box_basename=ccdc-basebox/ubuntu-18.04' \
+  ./ubuntu-18.04-amd64.json
+
+mv output/ccdc-basebox/ubuntu-18.04.virtualbox.box output/ccdc-basebox/ubuntu-18.04.$(date +%Y%m%d).0.virtualbox.box
---
+++ build-vmware.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+pushd $DIR
+
+echo 'creating output directory'
+mkdir -p output
+
+echo 'cleaning up intermediate output'
+rm -rf ./output/packer-ubuntu-18.04-amd64-vmware
+
+echo 'building base images'
+packer build \
+  -only=vmware-iso \
+  -except=vsphere,vsphere-template \
+  -var 'build_directory=./output/' \
+  -var 'disk_size=400000' \
+  -var 'cpus=2' \
+  -var 'memory=4096' \
+  -var 'vmx_remove_ethernet_interfaces=true' \
+  -var 'box_basename=ccdc-basebox/ubuntu-18.04' \
+  ./ubuntu-18.04-amd64.json
+
+
+mv output/ccdc-basebox/ubuntu-18.04.vmware.box output/ccdc-basebox/ubuntu-18.04.$(date +%Y%m%d).0.vmware_desktop.box
